7a8b75ba8058d63156109970bc1f1325897491b4
Steven <anthonystevenjulian@gmail.com>
2025-07-04 00:48:12 -0400

    correcion_bg_footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-blue-900 py-6">
+    <footer className="bg-gray-900 py-6">
       <div className="container mx-auto px-6 text-center">
         <p className=" font-semibold text-white text-sm">created by Steven</p>
       </div>
